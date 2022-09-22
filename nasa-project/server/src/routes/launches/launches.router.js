c38f4411fa1866e6a6a39e32c9d4bc53982c6a0d
WinterNightSkies <richierichzap@gmail.com>
2022-09-22 14:45:19 -0500

    finishing up nasa-project rest api's

--- nasa-project/server/src/routes/launches/launches.router.js
+++ nasa-project/server/src/routes/launches/launches.router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const {
-    getAllLaunches,
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch,
 } = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', getAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
